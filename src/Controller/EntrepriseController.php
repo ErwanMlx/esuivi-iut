7fb47076972b4bc8e73036fedd56ab3c67c7c32f
ErwanPM <pm.erwan@gmail.com>
2018-03-03 16:05:55 +0100

    Ajout version statique ajout entreprise

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -10,14 +10,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 
-class DossierController extends Controller
+class EntrepriseController extends Controller
 {
     /**
-     * @Route("/bordereau/remplir", name="remplir_bordereau")
+     * @Route("/entreprise/informations/", name="infos_entreprise")
      */
-    public function remplir_bordereau()
+    public function infos_entreprise()
     {
-
+        return $this->render('entreprise/entreprise.html.twig');
     }
 
 
